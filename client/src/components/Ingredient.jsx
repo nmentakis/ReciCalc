cfa53f97cc30f4f2e54cd24970a63edd0b9bcf98
johnpwarren@gmail.com <johnpwarren@gmail.com>
2018-11-02 11:41:51 -0500

    added ingredients route and starting ingredients suggestions

--- client/src/components/Ingredient.jsx
+++ client/src/components/Ingredient.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import { Grid, Image } from 'semantic-ui-react'
 
-const RecipeSuggestion = (props) => (
+const Ingredient = (props) => (
   <Grid celled='internally'>
     <Grid.Row>
       <Grid.Column width={3}>
         <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
       </Grid.Column>
       <Grid.Column width={10}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-      </Grid.Column>
-      <Grid.Column width={3}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+        <Image src='https://react.semantic-ui.com/images/wireframe/centered-paragraph.png' />
       </Grid.Column>
     </Grid.Row>
   </Grid>
 )
 
-export default RecipeSuggestion
+export default Ingredient
 
